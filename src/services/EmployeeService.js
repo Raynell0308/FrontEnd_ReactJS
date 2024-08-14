4e95f9a7041caa24cec3f0bb31275aa8c6ec2cb7
Raynell0308 <annie7345k@gmail.com>
2024-08-14 09:23:43 +0900

    reactjs first commit

---
+++ src/services/EmployeeService.js
@@ -0,0 +1,6 @@
+import axios from 'axios'
+
+const REST_API_BASE_URL =  import.meta.env.VITE_APIURL;
+
+const DEPARTMENT_REST_API_URL = `${REST_API_BASE_URL}/departments`
+//'http://localhost:8080/api/departments'
